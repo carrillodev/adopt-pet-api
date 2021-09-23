a61da87fd1026c6624369918a71dc54f220a0731
Felipe Carrillo <felipe.carrillo1@hotmail.com>
2021-09-22 21:26:16 -0500

    Solve async/await bug with connection to DB

--- app.js
+++ app.js
@@ -7,15 +7,19 @@ require('./models/Mascota');
 require('./models/Solicitud');
 require('./config/passport');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+async function init() {
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI);
-mongoose.set('debug', true);
+  await mongoose.connect(process.env.MONGO_URI);
+  mongoose.set('debug', true);
 
-app.use('/v1', require('./routes'));
+  app.use('/v1', require('./routes'));
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server is litening on port ${PORT}`);
-});
+  const PORT = process.env.PORT;
+  app.listen(PORT, () => {
+    console.log(`Server is litening on port ${PORT}`);
+  });
+}
+
+init();
