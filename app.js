651b69734fa9069d3e139575f8f6fb052b849bf5
Felipe Carrillo <felipe.carrillo1@hotmail.com>
2021-09-22 21:35:37 -0500

    Fix connection

--- app.js
+++ app.js
@@ -8,18 +8,22 @@ require('./models/Solicitud');
 require('./config/passport');
 
 async function init() {
+  const PORT = process.env.PORT;
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
-  await mongoose.connect(process.env.MONGO_URI).catch(_ => null);
-  mongoose.set('debug', true);
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      mongoose.set('debug', true);
 
-  app.use('/v1', require('./routes'));
+      app.use('/v1', require('./routes'));
 
-  const PORT = process.env.PORT;
-  app.listen(PORT, () => {
-    console.log(`Server is litening on port ${PORT}`);
-  });
+      app.listen(PORT, () => {
+        console.log(`Server is litening on port ${PORT}`);
+      });
+    })
+    .catch(_ => null);
 }
 
 init();
